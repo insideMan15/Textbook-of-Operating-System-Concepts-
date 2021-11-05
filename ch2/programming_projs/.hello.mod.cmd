fc4292c8720e03fa9755261f3f98aea866633c32
Liu Quan <lq394023872@163.com>
2021-11-06 01:03:15 +0800

    updating ch2 programming projs

---
+++ ch2/programming_projs/.hello.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/test/os_answers/Textbook-of-Operating-System-Concepts-/ch2/programming_projs/hello.mod := { echo  /home/test/os_answers/Textbook-of-Operating-System-Concepts-/ch2/programming_projs/hello.o;  echo; } > /home/test/os_answers/Textbook-of-Operating-System-Concepts-/ch2/programming_projs/hello.mod

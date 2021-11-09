bf796e3f75d91a406d347ba43b4cf147781de7ba
Liu Quan <lq394023872@163.com>
2021-11-09 23:52:44 +0800

    update 3.18 programming exs

--- ch3/programming_exs/3_18.c
+++ ch3/programming_exs/3_18.c
@@ -1,5 +1,19 @@
-#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main() {
-	return 0;
+int main()
+{
+	pid_t pid;
+	pid = fork();
+	
+	if (pid == 0 ) { /*child process*/
+		fprintf("a zombie process: %u", pid);
+		return 0;
+	} else if(pid > 0){ /* parent process */
+		sleep(10);
+		fprintf("parent complete: %u", pid); /* LINE A */
+		return 0;
+	}
 }

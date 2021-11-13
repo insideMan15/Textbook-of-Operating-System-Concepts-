a0c66f09e633ec3ce0a8c5a0324c73f78f1f59ba
Liu Quan <lq394023872@163.com>
2021-11-14 00:55:58 +0800

    finish 3_19 shared memory IPC

--- ch3/programming_exs/3_19.c
+++ ch3/programming_exs/3_19.c
@@ -2,21 +2,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <sys/mman.h>
 
 #define BUFFER_SIZE 1
 
-int main() {
+struct timeval current;
+
+int main(int argc, char* argv[]) {
 	pid_t pid;
+	char* command = argv[1];
+	long *startStamp;
+	long nowStamp;
+	startStamp = mmap(NULL, sizeof *startStamp, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    struct timeval tv;
 	pid = fork();
-	int buffer[BUFFER_SIZE];
-	if (pid == 0 ) { /*child process*/
-		buffer[0] = 1;
-		printf("a zombie process: %u", pid);
+	if (pid == 0) {
+		*startStamp = tv.tv_sec;
+		printf("st: %lu \n", *startStamp);
+		execlp(command, command, ".", NULL);	
 		return 0;
-	} else if(pid > 0){ /* parent process */
+	} else if (pid > 0) {
+		nowStamp = tv.tv_sec;
 		wait(NULL);
-		int a = buffer[0];
-		printf("parent complete: %u", a); 
+		printf("et: %lu \n", nowStamp);
+		printf("used time:%lu \n", (nowStamp-*startStamp)/1000);
 		return 0;
 	}
 }
